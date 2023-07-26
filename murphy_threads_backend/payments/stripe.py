e885df6ad973e9bdcc9264dda205ffbe1a9dc668
TierGamerpy <adityabhushan934@gmail.com>
2023-07-26 16:26:35 +0530

    Update stripe.py

--- murphy_threads_backend/payments/stripe.py
+++ murphy_threads_backend/payments/stripe.py
@@ -2,7 +2,7 @@ import stripe
 from django.conf import settings
 
 
-if settings.DEBUG == True:
-    stripe.api_key = settings.STRIPE_LIVE_KEY
-else:
+if settings.IS_UNDER_DEVELOPMENT == True:
     stripe.api_key = settings.STRIPE_TEST_KEY
+else:
+    stripe.api_key = settings.STRIPE_LIVE_KEY
