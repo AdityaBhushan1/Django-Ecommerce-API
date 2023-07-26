68de11377b7596a8299302aaef2efff8e456b078
TierGamerpy <adityabhushan934@gmail.com>
2023-07-26 20:32:43 +0530

    Update admin.py

--- murphy_threads_backend/product/admin.py
+++ murphy_threads_backend/product/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from product.models import *
-
-# Register your models here.
-admin.site.register(Products)
-admin.site.register(ProductImages)
-admin.site.register(ProductCategory)
-admin.site.register(Size)
-admin.site.register(Color)
-admin.site.register(Inventory)
