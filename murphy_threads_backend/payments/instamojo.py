38abd57012acb37a44a36a205628456b4dc4140f
TierGamerpy <adityabhushan934@gmail.com>
2023-07-26 16:27:53 +0530

    Update instamojo.py

--- murphy_threads_backend/payments/instamojo.py
+++ murphy_threads_backend/payments/instamojo.py
@@ -1,4 +1,9 @@
 from instamojo_wrapper import Instamojo
 from django.conf import settings
 
-instamojo_api = Instamojo(api_key=settings.INSTAMOJO_PRIVATE_LIVE_API_KEY,auth_token=settings.INSTAMOJO_PRIVATE_LIVE_AUTH_TOKEN)
+
+
+if settings.IS_UNDER_DEVELOPMENT == True: 
+     instamojo_api = Instamojo(api_key=INSTAMOJO_PRIVATE_TEST_API_KEY,auth_token=INSTAMOJO_PRIVATE_TEST_AUTH_TOKEN)
+ else: 
+     instamojo_api = Instamojo(api_key=settings.INSTAMOJO_PRIVATE_LIVE_API_KEY,auth_token=settings.INSTAMOJO_PRIVATE_LIVE_AUTH_TOKEN)
