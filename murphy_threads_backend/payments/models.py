3bd58c251f93bd07a8a2addc16dc2d6129744480
TierGamerpy <adityabhushan934@gmail.com>
2023-07-26 16:07:48 +0530

    Update models.py

--- murphy_threads_backend/payments/models.py
+++ murphy_threads_backend/payments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from account.models import *
 
 # Create your models here.
+
+class SavedCards(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    uid = models.ForeignKey(Users,on_delete=models.CASCADE,to_field='uid',null=False)
+    card_number = models.BigIntegerField()
+    expiry_month = models.IntegerField(null = False)
