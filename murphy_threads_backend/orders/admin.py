7d400de5d24c58a975a706d9d365153c5cd39007
TierGamerpy <adityabhushan934@gmail.com>
2023-07-26 20:30:38 +0530

    Update admin.py

--- murphy_threads_backend/orders/admin.py
+++ murphy_threads_backend/orders/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from orders.models import *
-
-# Register your models here.
-admin.site.register(Orders)
-admin.site.register(OrderState)
-admin.site.register(Refunds)
-admin.site.register(Returns)
