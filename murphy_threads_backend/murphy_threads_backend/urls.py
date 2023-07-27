f4127fa1665de17b83670252b09ec4e1ad020315
TierGamerpy <adityabhushan934@gmail.com>
2023-07-27 16:32:46 +0530

    added url path for all the apps and home page

--- murphy_threads_backend/murphy_threads_backend/urls.py
+++ murphy_threads_backend/murphy_threads_backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
+    # path("user/", include("account.urls"),name = 'user'),
+    # path("cart/", include("cart.urls"),name = 'cart'),
+    # path("extra/", include("extra.urls"),name = 'extra'),
+    # path("order/", include("orders.urls"),name = 'order'),
+    # path("payment/", include("payments.urls"),name = 'payment'),
+    # path("product/", include("product.urls"),name = 'product')
 ]
