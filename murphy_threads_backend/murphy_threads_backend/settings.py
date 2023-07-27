3592e7c42d6c4356f3000c59b590e8130792450e
TierGamerpy <adityabhushan934@gmail.com>
2023-07-27 16:32:17 +0530

    fixed some permissions error for rest framework

--- murphy_threads_backend/murphy_threads_backend/settings.py
+++ murphy_threads_backend/murphy_threads_backend/settings.py
@@ -137,11 +137,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 
