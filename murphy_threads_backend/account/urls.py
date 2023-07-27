47d8c36d9298b28b7516f809b4fb6b321a7c7fff
Aditya Bhushan <adityabhushan934@gmail.com>
2023-07-27 18:09:40 +0530

    Update urls.py

--- murphy_threads_backend/account/urls.py
+++ murphy_threads_backend/account/urls.py
@@ -6,5 +6,8 @@ from .views import *
      path("", account_index,name="user_home_page"), 
      path('/get-user-by-username/<str:username>', get_user_by_username, name='get_user_by_username'),
 path("/get-user-by-id/<int:pk>",get_user_by_id,name="get_user_by_id"),
-path('/get_user_addresses/<int:user_id>',get_user_addresses,name="get_user_addresses"),
+path('/get_user_addresses/<int:uid>',get_user_addresses,name="get_user_addresses"),
+path('/create-user/',create_new_user,name="create_new_user"),
+path("/update-user/<int:pk>",update_user,name="update_user"),
+path('/delete-user/<int:id>',delete_user,name="delete_user"),
 ]
