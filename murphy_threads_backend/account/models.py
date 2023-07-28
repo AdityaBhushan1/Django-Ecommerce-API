d5a8b2a34f368b2a0fb4f79fa245a81f4dbcfdb3
Aditya Bhushan <adityabhushan934@gmail.com>
2023-07-28 15:43:56 +0530

    Update models.py

--- murphy_threads_backend/account/models.py
+++ murphy_threads_backend/account/models.py
@@ -6,7 +6,7 @@ from .manager import UserManager
 
 # Create your models here.
 class Users(AbstractBaseUser, PermissionsMixin):      
-    username = models.CharField(primary_key=True,max_length=255,null=False,unique=True)
+    username = models.CharField(primary_key=True,max_length=255,unique=True)
    # password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255,null=False)
     last_name = models.CharField(max_length=255,null=False)
