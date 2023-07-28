02f59a39144c62839b08edc11d37be4c49d33e0c
Aditya Bhushan <adityabhushan934@gmail.com>
2023-07-28 16:31:17 +0530

    Update urls.py

--- murphy_threads_backend/account/urls.py
+++ murphy_threads_backend/account/urls.py
@@ -10,23 +10,21 @@ urlpatterns = [
 
      path('get-user-by-username/<str:username>', views.get_user_by_username, name='get_user_by_username'),
 
-     path("get-user-by-id/<int:pk>",views.get_user_by_id,name="get_user_by_id"),
-
-     path('get_user_addresses/<int:uid>',views.get_user_addresses,name="get_user_addresses"),
+     path('get_user_addresses/<str:username>',views.get_user_addresses,name="get_user_addresses"),
 
      path('create-user/',views.create_new_user,name="create_new_user"),
      
-     path("update-user/<int:pk>",views.update_user,name="update_user"),
+     path("update-user/<str:username>",views.update_user,name="update_user"),
      
-     path('delete-user/<int:id>',views.delete_user,name="delete_user"),
+     path('delete-user/<str:username>',views.delete_user,name="delete_user"),
      
-     path("account/verify/<str:username>/<int:pk>/<str:email_token>",views.activate_account,name="account_activation"),
+     path("account/verify/<str:username>/<str:email_token>",views.activate_account,name="account_activation"),
 
-     path('request-password-reset/<int:pk>',views.request_password_reset,name="pass_reset_request"),
+     path('request-password-reset/<str:username>',views.request_password_reset,name="pass_reset_request"),
 
      path('password-reset/<str:pass_reset_token>',views.reset_pass,name="pass_reset"),
 
-     path('add-address/<int:id>',views.add_new_address,name="add_new_address"),
+     path('add-address/,views.add_new_address,name="add_new_address"),
      
      path('update-address/<int:id>',views.update_address,name="address_update"),
 
