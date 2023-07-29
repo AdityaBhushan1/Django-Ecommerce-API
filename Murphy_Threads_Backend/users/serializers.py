5a465bef3c145a9687e0bd2539bbc263803d2868
TierGamerpy <adityabhushan934@gmail.com>
2023-07-30 00:29:06 +0530

    added userlogin searilizers

--- Murphy_Threads_Backend/users/serializers.py
+++ Murphy_Threads_Backend/users/serializers.py
@@ -35,3 +35,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         return Users.objects.create_user(**validate_data)
 
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255)
+    class Meta:
+        model = Users
+        fields = [
+            'email', 
+            'password'
+        ]
