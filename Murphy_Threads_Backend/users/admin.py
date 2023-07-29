8f11df621163a2d95be2966eab4cf254f8c3e63c
TierGamerpy <adityabhushan934@gmail.com>
2023-07-29 22:56:12 +0530

    added UserModelAdmin for admin panel

--- Murphy_Threads_Backend/users/admin.py
+++ Murphy_Threads_Backend/users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Users
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User 
 
 # Register your models here.
+class UserModelAdmin(UserAdmin):
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserModelAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["email", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["first_name","last_name","phone_no"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email","first_name","last_name","phone_no", "password"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+admin.site.register(Users, UserModelAdmin)
