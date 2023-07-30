d39d11c18bd456621aefa7fd11e1d54fef872c09
TierGamerpy <adityabhushan934@gmail.com>
2023-07-30 09:37:39 +0530

    added password change for logged in users

--- Murphy_Threads_Backend/users/urls.py
+++ Murphy_Threads_Backend/users/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('register/',UserRegistrationView.as_view(),name="register"),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     
     # path('get-user-by-username/<str:username>', views.get_user_by_username, name='get_user_by_username'),
 
--- Murphy_Threads_Backend/users/views.py
+++ Murphy_Threads_Backend/users/views.py
@@ -72,3 +72,21 @@ class UserProfileView(APIView):
             serializer.data, 
             status=status.HTTP_200_OK
             ) 
+
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(
+            data=request.data, 
+            context={
+                'user':request.user
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        return Response(
+            {
+                'msg':'Password Changed Successfully'
+            }, 
+            status=status.HTTP_200_OK
+        )
