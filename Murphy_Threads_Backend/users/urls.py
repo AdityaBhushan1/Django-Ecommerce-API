8d803674e7b9d0d985e289424f065ba981d4bf83
TierGamerpy <adityabhushan934@gmail.com>
2023-07-30 20:34:35 +0530

    added is_authentication checks for authenticated users

--- Murphy_Threads_Backend/users/urls.py
+++ Murphy_Threads_Backend/users/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('is_authenticated', TokenVerifyView.as_view(), name='is_authenticated'),
     path('register/',UserRegistrationView.as_view(),name="register"),
     # path('account/activate/<str:uid>/<str:token>/', ActivateView.as_view(), name='activate'),
     path('activate/', ActivationConfirm.as_view(), name='activate'),
