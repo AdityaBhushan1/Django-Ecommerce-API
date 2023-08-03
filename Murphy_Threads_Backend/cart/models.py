9813a3a1e020723b47f6c42f765789802067d9c5
Aditya Bhushan <adityabhushan934@gmail.com>
2023-08-03 16:23:26 +0530

    Update models.py

--- Murphy_Threads_Backend/cart/models.py
+++ Murphy_Threads_Backend/cart/models.py
@@ -4,7 +4,9 @@ from products.models import Products
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(Users,on_delete=models.CASCADE)
-    product = models.ForeignKey(Products,on_delete=models.CASCADE)
+    product = models.ForeignKey(Products,on_delete=models.CASCADE,null=True)
+    is_variated_product = models.BooleanField(default=False)
+    variated_product=models.ForeignKey(ProductsVariations,on_delete=models.CASCADE, null =True)
     quantity = models.PositiveIntegerField(default = True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
