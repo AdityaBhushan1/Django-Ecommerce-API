1e3086771cae049ce15425e11f3788a8fc89834f
TierGamerpy <adityabhushan934@gmail.com>
2023-08-03 12:33:54 +0530

    add cart crud functionalities

--- Murphy_Threads_Backend/cart/models.py
+++ Murphy_Threads_Backend/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from users.models import Users
+from products.models import Products
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(Users,on_delete=models.CASCADE)
+    product = models.ForeignKey(Products,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
