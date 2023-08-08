8d627df67a631388cebbcf57b199f665aed44fe8
Aditya Bhushan <adityabhushan934@gmail.com>
2023-08-08 16:13:50 +0530

    Update models.py

--- Murphy_Threads_Backend/orders/models.py
+++ Murphy_Threads_Backend/orders/models.py
@@ -18,17 +18,22 @@ ORDER_STATUS_CHOICES = (
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, to_field='email',null=False)
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     shipping_address_id = models.ForeignKey(UserAddresses, on_delete=models.CASCADE)
     status = models.CharField(choices=ORDER_STATUS_CHOICES, default='PENDING', max_length=20)
     status_message = models.CharField(max_length = 500,null = True)
     ammount_paid = models.DecimalField(max_digits = 20,decimal_places = 2,null = True)
-    quantity = models.IntegerField(default = 1)
     shipped_date = models.DateTimeField(null = True)
     delivered_date = models.DateTimeField(null = True)
     ordered_on = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+class OrderItem():
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    def __str__(self):
+        return self.product.name
 
 # class Refund(models.Model):
 #     order = models.OneToOneField(Order, on_delete=models.CASCADE)
